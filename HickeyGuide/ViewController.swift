bd09c01feb7eab43d2c5bc6442e088667ea40b74
Thomas Tao <jiachen215@qq.com>
2016-04-05 13:04:05 +0800

    Initial Commit

---
+++ HickeyGuide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HickeyGuide
+//
+//  Created by Tao Jiachen on 16/4/5.
+//  Copyright © 2016年 HickeyTech Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ HickeyGuideTests/HickeyGuideTests.swift
@@ -0,0 +1,36 @@
+//
+//  HickeyGuideTests.swift
+//  HickeyGuideTests
+//
+//  Created by Tao Jiachen on 16/4/5.
+//  Copyright © 2016年 HickeyTech Inc. All rights reserved.
+//
+
+import XCTest
+@testable import HickeyGuide
+
+class HickeyGuideTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
